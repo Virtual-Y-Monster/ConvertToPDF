d9f6a7529cdaad03a651eae9252354af92cf82f6
Virtual-Y-Monster <1113459606@qq.com>
2020-10-12 23:45:14 +0800

    完成控件触发事件

--- PPTtoPDF.py
+++ PPTtoPDF.py
@@ -1,6 +1,8 @@
 import comtypes.client
 import os
 
+__all__ = ["PPTtoPDF"]
+
 
 def init_ppt():
     powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
@@ -15,28 +17,25 @@ def ppt_to_pdf(powerpoint, inputFile, outputFile):
 
 
 def convert_ppts_in_folder(powerpoint, inputfolder, outputfolder):
+    files = os.listdir(inputfolder)
+    pptfiles = [f for f in files if f.endswith((".ppt", ".pptx", ".PPT"))]
+    for pptfile in pptfiles:
+        inputFile = os.path.join(inputfolder, pptfile)
+        outFileName = ""
+        if pptfile.endswith(("ppt", "PPT")):
+            outFileName = pptfile[:-3] + "pdf"
+        elif pptfile.endswith("pptx"):
+            outFileName = pptfile[:-4] + "pdf"
+        outputFile = os.path.join(outputfolder, outFileName)
+        ppt_to_pdf(powerpoint, inputFile, outputFile)
+
+
+def PPTtoPDF(inputfolder, outputfolder):
+    powerpoint = init_ppt()
+    status = True
     if os.path.exists(inputfolder) and os.path.exists(outputfolder):
-        files = os.listdir(inputfolder)
-        pptfiles = [f for f in files if f.endswith((".ppt", ".pptx", ".PPT"))]
-        for pptfile in pptfiles:
-            inputFile = os.path.join(inputfolder, pptfile)
-
-            outFileName = ""
-            if pptfile.endswith(("ppt", "PPT")):
-                outFileName = pptfile[:-3] + "pdf"
-            elif pptfile.endswith("pptx"):
-                outFileName = pptfile[:-4] + "pdf"
-            outputFile = os.path.join(outputfolder, outFileName)
-
-            ppt_to_pdf(powerpoint, inputFile, outputFile)
-
-        return True
+        convert_ppts_in_folder(powerpoint, inputfolder, outputfolder)
     else:
-        return False
-
-
-if __name__ == '__main__':
-    ppt = init_ppt()
-    cwd = "E:\\code\\test"
-    convert_ppts_in_folder(ppt, cwd, cwd)
-    ppt.Quit()
+        status = False
+    powerpoint.Quit()
+    return status
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import tkinter.filedialog
+import tkinter.messagebox
+from PPTtoPDF import *
 
 
 class Gui:
@@ -13,14 +15,18 @@ class Gui:
         self.toPath = StringVar()
         # 待转换文件路径
         self.lab1 = Label(self.top, text="文件路径：").grid(row=0, column=0, sticky=W, padx=5, pady=5)
-        self.entry1 = Entry(self.top, width=25, textvariable=self.fromPath).grid(row=0, column=1, sticky=W, padx=5, pady=5)
-        self.btn1 = Button(self.top, text="路径选择", command=self.selectFromPath).grid(row=0, column=2, sticky=W, padx=5, pady=5)
+        self.entry1 = Entry(self.top, width=25, textvariable=self.fromPath).grid(row=0, column=1, sticky=W, padx=5,
+                                                                                 pady=5)
+        self.btn1 = Button(self.top, text="路径选择", command=self.selectFromPath).grid(row=0, column=2, sticky=W, padx=5,
+                                                                                    pady=5)
         # PDF存放的目标路径
         self.lab2 = Label(self.top, text="目标路径:").grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        self.entry2 = Entry(self.top, width=25, textvariable=self.toPath).grid(row=1, column=1, sticky=W, padx=5, pady=5)
-        self.btn2 = Button(self.top, text="路径选择", command=self.selectToPath).grid(row=1, column=2, sticky=W, padx=5, pady=5)
+        self.entry2 = Entry(self.top, width=25, textvariable=self.toPath).grid(row=1, column=1, sticky=W, padx=5,
+                                                                               pady=5)
+        self.btn2 = Button(self.top, text="路径选择", command=self.selectToPath).grid(row=1, column=2, sticky=W, padx=5,
+                                                                                  pady=5)
 
-        self.btn3 = Button(self.top, text="转换").grid(row=2, column=2, sticky=W, padx=5, pady=5)
+        self.btn3 = Button(self.top, text="转换", command=self.btn3_click).grid(row=2, column=2, sticky=W, padx=5, pady=5)
 
         self.top.mainloop()
 
@@ -41,6 +47,19 @@ class Gui:
         path = tkinter.filedialog.askdirectory()
         if not path.isspace():
             self.toPath.set(path)
+
+    def btn3_click(self):
+        """
+        转换按钮点击事件
+        :return:
+        """
+        inputfolder = self.fromPath.get().replace("/", "\\")
+        outputfolder = self.toPath.get().replace("/", "\\")
+        status = PPTtoPDF(inputfolder, outputfolder)
+        if not status:
+            tkinter.messagebox.showerror(title="错误", message="路径错误！")
+        else:
+            tkinter.messagebox.showinfo(title="提示", message="转换完毕")
 
 
 if __name__ == '__main__':
